401b6fe092b5fd89616d65f46f73236f5274b038
cagdaskalsen <cagdaskalsen@hotmail.com>
2023-01-30 15:21:13 -0500

    Home page set up

---
+++ capstone_project/main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+
+# Create your views here.
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
